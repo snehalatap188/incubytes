6a17ffbd845d2afbbfdf7064d77c99956aa35c36
Swaraj <Swaraj@DESKTOP-3GR7K5M>
2021-01-24 12:26:14 +0530

    TestCase-2--For an empty string the method will return 0. Test Case 3-- Method will return their sum of numbers

--- IncubytesTDDProject/src/com/incubytes/test/StringCalculatorTestCase3Test.java
+++ IncubytesTDDProject/src/com/incubytes/test/StringCalculatorTestCase3Test.java
@@ -7,13 +7,12 @@ import junit.framework.Assert;
 
 //Test case 3-- Method will Test their sum of numbers
 public class StringCalculatorTestCase3Test {
-
-	
+	//when One Number Is Used Then Return Value Is That Same Number
 	@Test
 	public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
 	    Assert.assertEquals(3, StringCalculatorTestCase3.add("3"));
 	}
-
+	//when Two Numbers Are Used Then Return Value Is Their Sum
 	@Test
 	public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
 	    Assert.assertEquals(3+6, StringCalculatorTestCase3.add("3,6"));
